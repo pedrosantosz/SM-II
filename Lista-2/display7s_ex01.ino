93ea09b4cbd8062480e42d257fb300fc21991622
Pedro Santos <89491903+pedrosantosz@users.noreply.github.com>
2023-05-05 00:28:08 -0300

    Create display7s_ex01.ino

---
+++ Lista-2/display7s_ex01.ino
@@ -0,0 +1,24 @@
+#define DATA 5
+#define CLOCK 7
+#define LATCH 6
+
+void setup() {
+  pinMode(DATA, OUTPUT);
+  pinMode(CLOCK, OUTPUT);
+  pinMode(LATCH, OUTPUT);
+
+  digitalWrite(CLOCK, LOW);
+  digitalWrite(LATCH, LOW);
+
+  shiftOut(DATA, CLOCK, LSBFIRST, B11101111);
+  shiftOut(DATA, CLOCK, LSBFIRST, B11011010);
+  enviaPulso(LATCH);
+}
+
+void loop() {
+}
+
+void enviaPulso(int pino) {
+  digitalWrite(pino, HIGH);
+  digitalWrite(pino, LOW);
+}
