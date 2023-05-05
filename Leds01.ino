6bf19458f565e896b7ef956b265ba871e5fac8fd
Pedro Santos <89491903+pedrosantosz@users.noreply.github.com>
2023-05-05 00:20:09 -0300

    Create Leds01.ino

---
+++ Leds01.ino
@@ -0,0 +1,35 @@
+#define DATA 2
+#define LATCH 3
+#define CLOCK 4
+
+void setup(){
+  pinMode(DATA, OUTPUT);
+  pinMode(LATCH, OUTPUT);
+  pinMode(CLOCK, OUTPUT);
+  
+  digitalWrite(CLOCK, 0); // (estado
+  digitalWrite(LATCH, 0); //  inicial)
+
+  desligaLeds();
+  
+  digitalWrite(DATA, 1);
+  
+  digitalWrite(CLOCK, 1);
+  digitalWrite(CLOCK, 0);
+  
+  digitalWrite(LATCH, 1);
+}
+
+void loop(){
+  
+}
+
+void desligaLeds() {
+  digitalWrite(DATA, 0);
+  for (int i = 1; i <= 8; i++) {
+    digitalWrite(CLOCK, 1);
+    digitalWrite(CLOCK, 0);
+  }
+  digitalWrite(LATCH, 1);
+  digitalWrite(LATCH, 0);
+}
